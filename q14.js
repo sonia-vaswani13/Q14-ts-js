ee1ea8b733d8c268857964db58323b79305796a0
sonia-vaswani13 <163179929+sonia-vaswani13@users.noreply.github.com>
2024-04-29 13:05:54 -0700

    Q14 ts/js node js of 45 assignment
    Q14 ts/js node js of 45 assignment

---
+++ q14.js
@@ -0,0 +1,6 @@
+//Q14.Guest List: If you could invite anyone, living or deceased, to dinner, 
+//who would you invite? Make a list that includes at least three people you’d 
+//like to invite to dinner. Then use your list to print a message to each person,
+// inviting them to dinner.
+var frndsArr = ["Adv Haya", "Adv Bushra", "Adv Sidra", "Adv zareena"];
+frndsArr.map(function (item) { return (console.log("Dear ".concat(item, " , you are Invited to a Dinner "))); });
